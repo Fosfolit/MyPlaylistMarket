d9dd413767c080ba77d03597c2199f66e2e0b9d4
bedarev-egorka <bedarev.egorka@yandex.ru>
2025-01-04 16:07:38 +0800

    working in code

--- app/src/main/java/com/example/playlistmarket/MainActivity.kt
+++ app/src/main/java/com/example/playlistmarket/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmarket
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val Button1 = findViewById<Button>(R.id.buttonSearch)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        Button1.setOnClickListener(imageClickListener)
+
+        val Button2 = findViewById<Button>(R.id.buttonMedia)
+
+        Button2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопкуууууууууууууууу!", Toast.LENGTH_SHORT).show()
         }
     }
 }
