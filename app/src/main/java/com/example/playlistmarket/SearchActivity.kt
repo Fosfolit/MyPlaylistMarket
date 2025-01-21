4219daa6ddbd718ed34d7ea627783dbc59ae7b04
bedarev-egorka <bedarev.egorka@yandex.ru>
2025-01-21 17:54:03 +0800

    EditText и TextWatcher

--- app/src/main/java/com/example/playlistmarket/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmarket/SearchActivity.kt
@@ -1,6 +1,13 @@
 package com.example.playlistmarket
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -14,5 +21,37 @@ class SearchActivity : AppCompatActivity() {
         toolbar.setOnClickListener {
             finish()
         }
+
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                } else {
+
+                }
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
 }
--- app/src/main/java/com/example/playlistmarket/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmarket/SettingsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
@@ -19,14 +20,17 @@ class SettingsActivity : AppCompatActivity() {
         }
         val textViewStyle: Switch = findViewById(R.id.textViewStyle)
         textViewStyle.setOnClickListener {
+            if (textViewStyle.isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
           }
-
         val textViewShare: TextView = findViewById(R.id.textViewShare)
         textViewShare.setOnClickListener {
             val shareIntent = Intent(Intent.ACTION_SENDTO)
             startActivity(Intent.createChooser(shareIntent,"@string/titleYandex"))
         }
-
         val buttonHelper: TextView = findViewById(R.id.buttonHelper)
         buttonHelper.setOnClickListener {
         val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
@@ -35,7 +39,6 @@ class SettingsActivity : AppCompatActivity() {
         emailIntent.putExtra(Intent.EXTRA_TEXT, "@string/spsPeople")
         startActivity(emailIntent)
         }
-
         val buttonUserText: TextView = findViewById(R.id.buttonUserText)
         buttonUserText.setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("@string/titleYandexText"))
