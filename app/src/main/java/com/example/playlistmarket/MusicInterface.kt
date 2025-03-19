e77c9f45e3804590948ce044dfa96ca1ee161dd0
bedarev-egorka <bedarev.egorka@yandex.ru>
2025-03-19 15:14:27 +0800

    рабочая база

---
+++ app/src/main/java/com/example/playlistmarket/MusicInterface.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmarket
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MusicInterface {
+    @GET("/search?entity=song ")
+    fun getMusic(@Query("term") query: String): Call<ListDataMusic>
+
+}
--- app/src/main/java/com/example/playlistmarket/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmarket/SearchActivity.kt
@@ -16,77 +16,82 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class SearchActivity : AppCompatActivity() {
+
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://itunes.apple.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create<MusicInterface>()
+
+
     private var countValue: String = ""
     private lateinit var inputEditText: EditText
+    private lateinit var clearButton :ImageView
+    private lateinit var recyclerView :RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
+        clearButton = findViewById<ImageView>(R.id.clearIcon)
         inputEditText = findViewById(R.id.inputEditText)
-        val clearButton   = findViewById<ImageView>(R.id.clearIcon)
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        clearButton.setOnClickListener {
-            hideKeyboardAndClearFocus(inputEditText)
-            inputEditText.setText("")
-        }
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
+        Textsee()
+
+        buttonclear()
+        toolFinish()
+    }
+
+    private fun Textne(inputtrext : String){
+        retrofit.getMusic(inputtrext).enqueue(object : Callback<ListDataMusic> {
+            override fun onResponse(call: Call<ListDataMusic>, response: Response<ListDataMusic>) {
+                if (response.isSuccessful) {
+                    val newsAdapter = MusicAdapter(response.body()!!.results)
+                    recyclerView.adapter = newsAdapter
+                } else {
+                }
+            }
+
+            override fun onFailure(call: Call<ListDataMusic>, t: Throwable) {
+
+            }
+        })
+    }
+
+
+
+    private fun Textsee(){
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun afterTextChanged(s: Editable?) {
                 countValue =  inputEditText.text.toString()
+                Textne(countValue)
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
             }
         }
-
         inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
 
-        val musicList = listOf(
-            DataMusic(
-                trackName = "Smells Like Teen Spirit",
-                artistName = "Nirvana",
-                trackTime = "5:01",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            DataMusic(
-                trackName = "Billie Jean",
-                artistName = "Michael Jackson",
-                trackTime = "4:35",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            DataMusic(
-                trackName = "Stayin' Alive",
-                artistName = "Bee Gees",
-                trackTime = "4:10",
-                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            DataMusic(
-                trackName = "Whole Lotta Love",
-                artistName = "Led Zeppelin",
-                trackTime = "5:33",
-                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            DataMusic(
-                trackName = "Sweet Child O'Mine",
-                artistName = "Guns N' Roses",
-                trackTime = "5:03",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg "
-            )
-        )
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val newsAdapter =MusicAdapter(musicList)
-        recyclerView.adapter = newsAdapter
-
-
-
-        toolFinish()
+    private fun buttonclear(){
+        clearButton.setOnClickListener {
+            hideKeyboardAndClearFocus(inputEditText)
+            inputEditText.setText("")
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
