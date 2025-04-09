191e8a6b968342fabafb8cbfac7259494a249cbc
bedarev-egorka <bedarev.egorka@yandex.ru>
2025-04-09 15:38:33 +0800

    увеличение да 10 треков

--- app/src/main/java/com/example/playlistmarket/RecentlyViewed.kt
+++ app/src/main/java/com/example/playlistmarket/RecentlyViewed.kt
@@ -1,15 +1,14 @@
 package com.example.playlistmarket
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
-import androidx.recyclerview.widget.ConcatAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import java.util.LinkedList
 
 
 class RecentlyViewed (var context: Context) {
+
+    private val maxListSize :Int = 10
     private val linkedList : LinkedList<DataMusic> = LinkedList()
     private val sharedPrefs: SharedPreferences = context.getSharedPreferences(
         PRACTICUM_EXAMPLE_PREFERENCES,
@@ -21,7 +20,7 @@ class RecentlyViewed (var context: Context) {
     fun addItem(item: DataMusic) {
         linkedList.remove(item)
 
-        if (linkedList.size >= 5) {
+        if (linkedList.size >= maxListSize) {
             linkedList.removeLast()
         }
 
@@ -31,7 +30,7 @@ class RecentlyViewed (var context: Context) {
 
     private fun loadInPhone(){
         linkedList.clear()
-        for(i in 0..5){
+        for(i in 0..maxListSize){
             val json =  sharedPrefs.getString(i.toString(), null) ?: return
             linkedList.offerLast(Gson().fromJson(json, DataMusic::class.java))
         }
@@ -50,14 +49,11 @@ class RecentlyViewed (var context: Context) {
         sharedPrefs.edit().clear().apply()
     }
     private fun clearload() {
-        sharedPrefs.edit()
-            .remove("0")
-            .remove("1")
-            .remove("2")
-            .remove("3")
-            .remove("4")
-            .remove("5")
-            .apply()
+        val clern = sharedPrefs.edit()
+        for( i in 0..maxListSize) {
+            clern.remove(i.toString())
+        }
+        clern.apply()
     }
     fun clearPreferencesAll() {
         clearload()
